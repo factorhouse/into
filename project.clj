857b9d2724a0368afb6b25b7387fa9176fdc4963
Derek Troy-West <derek@factorhouse.io>
2026-03-18 12:52:06 +1100

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.factorhouse/into "1.0.0"
 
-  :description "A hurdle of the technical variety"
+  :description "Factor House Coding Exercise"
 
   :dependencies [[org.clojure/clojure "1.12.4"]
                  [org.clojure/tools.logging "1.3.1"]
